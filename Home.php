2c98860d28839b136a69469cd4332953b2999a64
Shariar Rahman Shawon <158124558+ShariarRahmanShawon@users.noreply.github.com>
2025-03-16 01:59:53 +0600

    Add files via upload

--- Home.php
+++ Home.php
@@ -2,12 +2,10 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=, initial-scale=1.0">
     <title>Document</title>
 </head>
 <body>
-    <center>
-    <h2>Thank You for Submitting .....</h2>
-</center>
+    <center><h2>Thank You for registration</h2></center>
 </body>
 </html>
